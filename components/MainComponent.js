df70cee99907c14eb21302afdce55f66928f2251
George Gomez <george.gomez922@gmail.com>
2022-03-14 14:46:09 -0700

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -15,6 +15,7 @@ import {connect} from 'react-redux';
 import {fetchCampsites, fetchComments, fetchPromotions, fetchPartners} from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -144,6 +145,29 @@ const ReservationNavigator = createStackNavigator(
     }
 )
 
+const LoginNavigator = createStackNavigator(
+    {
+        Login: { screen: Login },
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='sign-in'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+)
+
 const FavoritesNavigator = createStackNavigator(
     {
         Favorites: { screen: Favorites },
@@ -191,6 +215,19 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
     {
+        Login: { 
+            screen: LoginNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='sign-in'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
         Home: { 
             screen: HomeNavigator,
             navigationOptions: {
@@ -275,6 +312,7 @@ const MainNavigator = createDrawerNavigator(
         }
     },
     {
+        initialRouteName: 'Home',
         drawerBackgroundColor: '#CEC8FF',
         contentComponent: CustomDrawerContentComponent
     }
